5d4574ddd20955ac3db0d03bbcc130e87e9add8e
Mather <409915016@qq.com>
2017-05-12 10:06:59 +0800

    * stop

--- src/components/UserHomeNone.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h3>Nothing in here</h3>
-</template>
-
-<script>
-  export default {
-    name: 'userhomenone'
-  }
-</script>
--- src/main.js
+++ src/main.js
@@ -1,16 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-//import App from './App'
 import VueRouter from "vue-router";
 import VueResource from "vue-resource";
-import UserHome from "./components/UserHome.vue";
-import UserPost from "./components/UserPost.vue";
-import layout_header from "./components/layout/header.vue";
-import layout_main from "./components/layout/main.vue";
-import layout_footer from "./components/layout/footer.vue";
 
-import UserHomeNone from "./components/UserHomeNone.vue";
+import Home from './components/Home'
+
 import "bootstrap/dist/css/bootstrap.css";
 
 Vue.use(VueRouter)
@@ -18,37 +13,20 @@ Vue.use(VueResource)
 
 Vue.config.productionTip = false
 
+const routes = [{
+  path : '/',
+  component : Home
+},{
+  path : '/home',
+  component : Home
+}];
+
 const router = new VueRouter({
-  routes: [
-    {
-      path: '/',
-      components: {
-        header: layout_header,
-        main: layout_main,
-        footer: layout_footer
-      }
-    },
-    {
-      path: '/user/:username',
-      component: UserHome,
-      name: 'userHome',
-      children: [
-        {path: '', component: UserHomeNone},
-        {
-          // 当 /user/:id/posts 匹配成功
-          // UserPosts 会被渲染在 User 的 <router-view> 中
-          path: 'post/:postMsg',
-          component: UserPost
-        }
-      ]
-    }
-  ]
-})
+  routes
+});
 
-/* eslint-disable no-new */
 const app = new Vue({
-  router
-}).$mount('#app')
-
-router.push({name: 'user', params: {userId: 123}})
-
+  el: '#app',
+  router,
+  ...App,
+});
