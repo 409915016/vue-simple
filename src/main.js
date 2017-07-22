e07d338c5ba13ead8a644095072f2e2c9f37d69f
Mather <409915016@qq.com>
2017-07-22 23:16:44 +0800

    * Setting up Routing.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from "vue";
 import App from "./App.vue";
 import VueResource from "vue-resource";
+import VueRouter from "vue-router";
+import Routes from "./router/routes";
 // import Ninjas from "./Ninjas.vue"
 // Vue.component('ninjas', Ninjas);
 Vue.config.productionTip = false
@@ -11,6 +13,12 @@ Vue.config.productionTip = false
 export const bus = new Vue();
 
 Vue.use(VueResource);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: Routes
+})
+
 //Costom directive
 // Vue.directive('rainbow', {
 //   bind(el, binding, vnode){
@@ -42,6 +50,18 @@ Vue.filter('snippet', function (value) {
 })
 
 
+
+const app = new Vue({
+  el: '#app',
+  render: h => h(App),
+  router: router,
+  data: {},
+  methods: {},
+  computed: {}
+});
+
+
+
 Vue.component('greeting', {
   template: '<p>name: {{ name }} <button v-on:click="changeName" > change name </button> </p>',
   data: function () {
@@ -55,71 +75,3 @@ Vue.component('greeting', {
     }
   }
 });
-
-const app = new Vue({
-  el: '#app',
-  render: h => h(App),
-  data: {
-    x: 0,
-    y: 0,
-    age: 23,
-    a: 0,
-    b: 0,
-    available: false,
-    nearby: false,
-    error: false,
-    success: false,
-    name: "Mather",
-    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
-    ninjas: [
-      {name: 'Ryu', age: 25},
-      {name: 'Yoshi', age: 35},
-      {name: 'Ken', age: 55},
-    ],
-    output: 'Your fav food',
-
-
-  },
-  methods: {
-    add: function (inc) {
-      this.age += inc;
-    },
-    subtract: function (dec) {
-      this.age -= dec;
-    },
-    updateXY: function (event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click: function () {
-      alert("clicked");
-    },
-    logName: function () {
-    },
-    logAge: function () {
-    },
-    readRefs: function () {
-      console.log(this.$refs.test.innerText);
-      this.output = this.$refs.input.value;
-    }
-
-
-  },
-  computed: {
-
-    addToA: function () {
-      return this.a + this.age;
-    },
-    addToB: function () {
-      return this.b + this.age;
-    },
-    compClasses: function () {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
-  }
-});
-
-
